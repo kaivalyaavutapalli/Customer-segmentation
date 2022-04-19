43d0efb53582e4b7f8ab49254e853bf4862d5c1b
kaivalyaavutapalli <103962841+kaivalyaavutapalli@users.noreply.github.com>
2022-04-19 14:18:59 -0400

    Update 1
    Update 1

--- Clustering.py
+++ Clustering.py
@@ -2,19 +2,22 @@
 
 
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from math import sqrt
-
+from sklearn.decomposition import PCA
 
 class Clustering:
         
-    def __init__(self,df):
+    def __init__(self,df,Value1,Value2):
         self.df=df
+        self.value1=Value1
+        self.value2=Value2
     
-    def clusters(self,value1,value2):
-        MinimumRange = value1
-        MaximumRange = value2
+    def kmeans(self):
+        MinimumRange = self.value1
+        MaximumRange = self.value2
 
         InertiaScore= []
         RangeList = range(MinimumRange, MaximumRange)
@@ -32,36 +35,31 @@ class Clustering:
         
     
     def calculate_wcss(self):
-        min_range = 2
-        max_range = 10
         wcss = []
-        for n in range(min_range, max_range):
-            kmeans = KMeans(n_clusters=n,random_state=0)
+        for n in range(self.value1,self.value2):
+            kmeans = KMeans(n_clusters=n,init='k-means++',random_state=0)
             kmeans.fit(self.df)
             wcss.append(kmeans.inertia_)
-        
         return wcss
 
-
-    
     def optimal_number_of_clusters(self,var1):
        
-        min_range = 2
-        max_range = 10
+        min_range = self.value1
+        max_range = self.value2
         x1, y1 = min_range, var1[0]
         x2, y2 = max_range, var1[len(var1)-1]
 
-        distances = []
+        distance = []
         for i in range(len(var1)):
-            x0 = i+2
-            y0 = var1[i]
-            numerator = abs((y2-y1)*x0 - (x2-x1)*y0 + x2*y1 - y2*x1)
+            x = i+2
+            y = var1[i]
+            numerator = abs((y2-y1)*x - (x2-x1)*y + x2*y1 - y2*x1)
             denominator = sqrt((y2 - y1)**2 + (x2 - x1)**2)
-            distances.append(numerator/denominator)
+            distance.append(numerator/denominator)
     
-        return distances.index(max(distances)) + 2
+        return distance.index(max(distance)) + 2
     
-    def kmeans(self,label1,label2):
+    def ClusterModel1(self,label1,label2):
 
         sum_of_squares = self.calculate_wcss()
 
@@ -73,19 +71,41 @@ class Clustering:
         clusters = kmeans.fit_predict(self.df)
         self.df["label"] = clusters
      
-        fig = plt.figure(figsize=(21,10))
-        ax = fig.add_subplot(111, projection='3d')
-        ax.scatter( self.df[label1][self.df.label == 0], self.df[label2][self.df.label == 0], c='blue', s=60)
+        figure = plt.figure(figsize=(20,15))
+        axis = figure.add_subplot(111, projection='3d')
+        axis.scatter( self.df[label1][self.df.label == 0], self.df[label2][self.df.label == 0], c='blue', s=50)
 
-        ax.scatter( self.df[label1][self.df.label == 1], self.df[label2][self.df.label == 1], c='red', s=60)
-        ax.scatter( self.df[label1][self.df.label == 2], self.df[label2][self.df.label == 2], c='green', s=60)
-        ax.scatter( self.df[label1][self.df.label == 3], self.df[label2][self.df.label == 3], c='orange', s=60)
+        axis.scatter( self.df[label1][self.df.label == 1], self.df[label2][self.df.label == 1], c='red', s=50)
+        axis.scatter( self.df[label1][self.df.label == 2], self.df[label2][self.df.label == 2], c='green', s=50)
+        axis.scatter( self.df[label1][self.df.label == 3], self.df[label2][self.df.label == 3], c='orange', s=50)
 
-        ax.view_init(30, 185)
+        axis.view_init(35, 140)
         plt.show()
-
         
+    def ClusterModel2(self,label1,label2):
+        sum_of_squares = self.calculate_wcss()
+        n = self.optimal_number_of_clusters(sum_of_squares)
+        pca = PCA(n_components=n)
+        principalComponents = pca.fit_transform(self.df)
 
-    
+        features = range(pca.n_components_)
+        plt.bar(features, pca.explained_variance_ratio_, color='black')
+        plt.xlabel('PCA features')
+        plt.ylabel('variance %')
+        plt.xticks(features)
 
-    
+        PCA_components = pd.DataFrame(principalComponents)
+        ks = range(self.value1, self.value2)
+        inertias = []
+
+        for k in ks:
+            model = KMeans(n_clusters=k)
+            model.fit(PCA_components.iloc[:,:2])
+            inertias.append(model.inertia_)
+
+        plt.plot(ks, inertias, '-o', color='black')
+        plt.xlabel('number of clusters, k')
+        plt.ylabel('inertia')
+        plt.xticks(ks)
+        plt.show()
+        model = KMeans(n_clusters=n)
--- Main.py
+++ Main.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 
 
+
+
 import pandas as pd
  
 from Clustering import *
 
+
 class Analysis:
     
-    df = pd.read_csv(r'C:\Users\bhavi\Downloads\Mall_Customers.csv')
-    
-               
-    print(df.head(10))
+    df = pd.read_csv(r'"C:\Desktop\programming\New folder1\Mall_Customers.csv"')
     
     # updating columns with null values to 0
     df=df.fillna(0)
@@ -18,19 +18,25 @@ class Analysis:
     # Converting categorical data to numeric
     df['Gender']=df['Gender'].replace(['Male', 'Female'],[0,1])
     
-    obj1= Clustering(df)
+    # Range of Clusters should be specified
+    min_range=2
+    max_range=10
+    
+    obj1= Clustering(df,min_range,max_range)
     
     # Specify range for clusters as  min_range and max_range
     
-    min_range=2
-    max_range=10
-    obj1.clusters(min_range,max_range)
+    obj1.kmeans()
     
     var1=obj1.calculate_wcss() 
     var2=obj1.optimal_number_of_clusters(var1)
     print("Number of optimal clusters is : "+ str(var2))
    
-    obj1.kmeans("Annual Income (k$)","Age")
-    obj1.kmeans("Age","Spending Score (1-100)")
-    obj1.kmeans("Annual Income (k$)","Spending Score (1-100)")
+    obj1.ClusterModel1("Annual Income (k$)","Age")
+    obj1.ClusterModel1("Age","Spending Score (1-100)")
+    obj1.ClusterModel1("Annual Income (k$)","Spending Score (1-100)")
+    
+
+    
+
     
